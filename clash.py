07468f5650350867f8c25208d9bc274a75fd856c
juankASL33 <98744511+juankASL33@users.noreply.github.com>
2024-06-14 11:27:54 +0200

    Update clash.py

--- clash.py
+++ clash.py
@@ -1,23 +1,36 @@
 import requests
+from dotenv import load_dotenv
+import os
 
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+# Token de autorización y encabezados
 headers = {
-    'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImU2ZmQ1ZmUyLTQwNDQtNGFlZC05NjBmLWQ0ZTE4YjVmYzk0ZiIsImlhdCI6MTcxNjk4MDY4MCwic3ViIjoiZGV2ZWxvcGVyL2IwMjMzYjU1LTczMDgtMjU2My01YjE0LWI5ZGE0OTk1ZmM3MCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjk1LjEyNy4xNTMuMjUzIl0sInR5cGUiOiJjbGllbnQifV19.-UPJECr3iSX3CcMC5L3bAi-xv41kSJsQrg9XfeWZHiWGHZERnzMz78gzCrWwXx7sIfo7k0KJIN9kUoQ-lvFXIA',  # Reemplaza 'tu_token_aqui' con tu token real
+    'Authorization': f'Bearer {os.getenv("CLASH_API_TOKEN")}',
+    'Content-Type': 'application/json',
     'Accept': 'application/json'
 }
 
 def get_user(player_tag):
-    url = f'https://api.clashofclans.com/v1/players/{player_tag}'
+    encoded_player_tag = player_tag.replace('#', '%23')
+    url = f'https://api.clashofclans.com/v1/players/{encoded_player_tag}'
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.json()
-    except requests.exceptions.HTTPError as http_err:
-        if response.status_code == 403:
-            raise Exception("Error 403: Acceso prohibido. Verifica tu token de autorización y las IPs permitidas.")
-        else:
-            raise Exception(f"HTTP error occurred: {http_err}")
-    except Exception as err:
-        raise Exception(f"Other error occurred: {err}")
+    except requests.exceptions.RequestException as e:
+        raise Exception(f"Error de conexión: {e}")
+
+def get_clan(clan_tag):
+    encoded_clan_tag = clan_tag.replace('#', '%23')
+    url = f'https://api.clashofclans.com/v1/clans/{encoded_clan_tag}'
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        raise Exception(f"Error de conexión: {e}")
 
 def search_clans(clan_name):
     url = f'https://api.clashofclans.com/v1/clans?name={clan_name}'
@@ -25,24 +38,5 @@ def search_clans(clan_name):
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.json()['items']
-    except requests.exceptions.HTTPError as http_err:
-        if response.status_code == 403:
-            raise Exception("Error 403: Acceso prohibido. Verifica tu token de autorización y las IPs permitidas.")
-        else:
-            raise Exception(f"HTTP error occurred: {http_err}")
-    except Exception as err:
-        raise Exception(f"Other error occurred: {err}")
-
-def get_clan(clan_tag):
-    url = f'https://api.clashofclans.com/v1/clans/{clan_tag}'
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.HTTPError as http_err:
-        if response.status_code == 403:
-            raise Exception("Error 403: Acceso prohibido. Verifica tu token de autorización y las IPs permitidas.")
-        else:
-            raise Exception(f"HTTP error occurred: {http_err}")
-    except Exception as err:
-        raise Exception(f"Other error occurred: {err}")
+    except requests.exceptions.RequestException as e:
+        raise Exception(f"Error de conexión: {e}")
